440aad20267964466eff6131d285595c07ac221e
Vignesh-victor <vvicky9671@gmail.com>
2026-07-31 17:59:53 +0530

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,9 @@
+a=int (input("enter your birth year"))
+b=int(input("what is yr 1 share price"))
+c=input("which country u like")
+d=input("what is your nick name ")
+print("ur age automaticcly generate",a-2026)
+print("this value for multiple to 500",b*500)
+print("oh my god best country in the world",c)
+print(d, "is nice name")
+
