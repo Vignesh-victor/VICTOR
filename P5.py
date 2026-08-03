dd899d68f0b3387c09913a6dd5ec07dc6c9f7947
Vignesh-victor <vvicky9671@gmail.com>
2026-08-03 13:27:01 +0530

    Add files via upload

---
+++ P5.py
@@ -0,0 +1,9 @@
+rank = int (input())
+if (rank>75):
+        print("good student")
+elif(rank>30 and rank<35):
+        print("just pass student")
+elif (rank<30):
+        print("bro is become a richest man")
+else:
+        print("avarage student")
