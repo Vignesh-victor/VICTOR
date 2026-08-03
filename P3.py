963fff3f605a23a959eec55530bf329e2a79bd8d
Vignesh-victor <vvicky9671@gmail.com>
2026-08-03 13:07:18 +0530

    Add files via upload

---
+++ P3.py
@@ -0,0 +1,11 @@
+score = int (input("type the mark"))
+
+if score >= 90:
+    print("Grade A")
+elif score >= 80:
+    print("Grade B")
+elif score <= 10:
+    print("ur is become a rice")
+else:
+    print("Keep studying!")
+
---
+++ P4.py
@@ -0,0 +1,5 @@
+a = int(input())
+if (a%3==0 and a%5==0):
+        print("divisible")
+else:
+        print("no divisible")
