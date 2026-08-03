bc22acab91eafebcc6af2be50c799643d9dffa32
Vignesh-victor <vvicky9671@gmail.com>
2026-08-03 17:16:21 +0530

    Add files via upload

---
+++ P7 new line  character.py
@@ -0,0 +1,12 @@
+sal = int(input())
+if(sal>=70):
+    
+    name=(input("name"))
+    age=(input("age"))
+    print("ur eligible")
+if (sal<=69):
+    print ("ur not eligible\nbut give ur name and age")
+    e=(input("name"))
+    t=(input("age"))
+    print("thanks you giveinga the feedback")
+    print("Keep Going\nThe road is long, the climb is steep,\nwith promises you swore to keep.\nJust take one step and do not rest,\nFor deep inside you lies the best.")
