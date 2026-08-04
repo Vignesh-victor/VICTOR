03181f18750aa858c28a63ffa95cf426d7d57a22
Vignesh-victor <vvicky9671@gmail.com>
2026-08-04 17:25:32 +0530

    Add files via upload

---
+++ P10   2 to 10 tables.py
@@ -0,0 +1,27 @@
+for i in range (1,11):
+    print(i,"x1=",i*2)
+    
+for i in range (1,11):
+    print(i,"x3=",i*3)
+    
+for i in range (1,11):
+    print(i,"4=",i*4)
+    
+for i in range (1,11):
+    print(i,"x5=",i*5)
+    
+for i in range (1,11):
+    print(i,"x6=",i*6)
+    
+for i in range (1,11):
+    print(i,"7=",i*7)
+    
+for i in range (1,11):
+    print(i,"x8=",i*8)
+    
+for i in range (1,11):
+    print(i,"x9=",i*9)
+    
+for i in range (1,11):
+    print(i,"10=",i*10)
+
