c4c48c27a372007800b9aba68cd5501aec2e8648
Vignesh-victor <vvicky9671@gmail.com>
2026-08-09 12:37:21 +0530

    Add files via upload

---
+++ P13 Calander using range.py
@@ -0,0 +1,8 @@
+for i in range(1,5):
+    print("Week",i)
+    for k in range(1,6):
+                            print("   Day",k,"Workdays")
+    for n in range(6,7):
+                            print("   Day",n,"Saturday")
+    for f in range(7,8):
+                            print("   Day",f,"Sunday")
