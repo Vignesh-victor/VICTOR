68eabe27aead06338414598f86e194f5f6080371
Vignesh-victor <vvicky9671@gmail.com>
2026-08-16 17:59:24 +0530

    Add files via upload

---
+++ P17 pyhton function basic.py
@@ -0,0 +1,48 @@
+def name():
+    print("hi vignesh")
+
+
+def vicky(o):
+    if (o>=35):
+        print("pass")
+    else:
+        print("fail")
+
+        print(o)
+v=int(input("enter the mark "))
+
+
+def vic(c,d):
+    for i in range (c,d):
+        print(i)
+
+
+def vi(e,f):
+    for i in range (e,f):
+        print(i)
+a=int(input("enter "))
+b=int(input("enter "))
+
+
+
+
+
+
+name()
+vicky(v)
+vic(1,11)
+vi(a,b)
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
