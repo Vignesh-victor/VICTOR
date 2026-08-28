9327f1bb9004d2c47e1a7c85dd1efd6005fbe255
Vignesh-victor <vvicky9671@gmail.com>
2026-08-28 18:27:39 +0530

    Add files via upload

---
+++ P19 class and object.py
@@ -0,0 +1,39 @@
+class tiger:
+    def __init__(self):
+        print("vignesh")
+
+    def lion(self):
+        self.name = "king of forest"
+        self.shot = "100 per day"
+        self.living = "forest"
+
+
+tiger()
+
+hp = tiger()
+hp.lion()
+
+print(hp.name)
+print(hp.shot)
+print(hp.living)
+
+
+
+class elephant:
+     def love(self):
+        self.name="kumki"
+        self.age =20
+        self.weight="1.50 ton"
+     def __init__(self):
+        print("one days poop miliion trees growing ")
+
+
+elephant()
+
+hi=elephant()
+hi.love()
+
+print(hi.name, hi.age, hi.weight)
+
+
+
